396b8ac1e77b3e6a53ff9de34986d4cd9a7fdda7
Sean Ho <seanho96@yahoo.com>
2022-03-12 13:54:19 +0800

    package: install semantic-ui

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
 
-module.exports = environment
+environment.plugins.append(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    Popper: ["popper.js", "default"],
+  })
+);
+environment.loaders.get("sass").use.splice(-1, 0, {
+  loader: "resolve-url-loader",
+});
+
+module.exports = environment;
