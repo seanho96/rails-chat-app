81e20930a297247c080cc0cd454e9d46ed9a308e
Sean Ho <seanho96@yahoo.com>
2022-03-21 07:46:07 +0800

    feat: build Message resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_320_225_841) do
+ActiveRecord::Schema.define(version: 20_220_320_234_246) do
+  create_table 'messages', force: :cascade do |t|
+    t.text 'body'
+    t.integer 'user_id'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+  end
+
   create_table 'users', force: :cascade do |t|
     t.string 'username'
     t.string 'password_digest'
