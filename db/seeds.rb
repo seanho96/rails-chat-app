f120f00fffc3eb57b2f7d72f2eafba51ffd3d9d4
Sean Ho <seanho96@yahoo.com>
2022-03-21 07:32:27 +0800

    feat: build User resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'Luke', password: 'password')
+User.create(username: 'Mike', password: 'password')
+User.create(username: 'Janet', password: 'password')
+User.create(username: 'Maria', password: 'password')
+User.create(username: 'Dave', password: 'password')
