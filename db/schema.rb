3ca3dbefbd6950b64bbb5816e779db4306911b03
Sean Ho <seanho96@yahoo.com>
2022-04-17 16:24:47 +0800

    refactor: apply code suggestions

--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(version: 20_220_320_234_246) do
   end
 
   create_table 'users', force: :cascade do |t|
-    t.string 'username'
-    t.string 'password_digest'
+    t.text 'username'
+    t.text 'password_digest'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: 'Luke', password: 'password')
-User.create(username: 'Mike', password: 'password')
-User.create(username: 'Janet', password: 'password')
-User.create(username: 'Maria', password: 'password')
-User.create(username: 'Dave', password: 'password')
+%w[Luke Mike Janet Maria Dave].each do |username|
+  User.create(username: username, password: 'password')
+end
