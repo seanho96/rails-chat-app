63357ddc0dfff4666596ea112e735d0a3aa912c7
Sean Ho <seanho96@yahoo.com>
2022-07-01 11:10:03 +0800

    chore: change message order

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :user
   validates :body, presence: true, length: { minimum: 1, maximum: 1000 }
-  scope :recent, -> { order(created_at: :desc).limit(12) }
+  scope :recent, -> { order(:created_at).last(12) }
 end
