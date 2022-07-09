a47c9907ab57bbfad8ea81fb54fd2f83a8c73e6b
Sean Ho <seanho96@yahoo.com>
2022-07-09 14:51:34 +0800

    chore: update websocket url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Action Cable config
-  config.action_cable.url = 'wss://rails-chatroom.herokuapp.com/cable'
+  config.web_socket_server_url = 'wss://rails-chatroom.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = ['https://rails-chatroom.herokuapp.com', 'http://rails-chatroom.herokuapp.com']
   # config.web_socket_server_url = 'wss://rails-chatroom.herokuapp.com/cable'
 
